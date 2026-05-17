56dd52b9b6bea4dc41196aa222f482bfce4fffeb
HAJAR-BOUYDAR/Kival <hajarbouydar@gmail.com>
2026-05-17 23:43:03 +0100

    fix: resolve CORS policy and configure correct allowed origins

--- laravel-api/config/cors.php
+++ laravel-api/config/cors.php
@@ -19,7 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+    ],
 
     'allowed_origins_patterns' => [],
 
